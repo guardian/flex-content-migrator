3179d553536bbe4e248513383cea63f09f154cf5
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2015-10-08 16:29:21 +0100

    app config to allow cartoon migration and conversion code

--- app/services/migration/R2ToFlexConversion.scala
+++ app/services/migration/R2ToFlexConversion.scala
@@ -179,7 +179,7 @@ class R2ToFlexGalleryConversion(jsonMap : Map[String, Any], parseLiveData : Bool
 
   import scala.language.postfixOps
 
-  private def explicit = getAsString("explicit").map(_.toBoolean).map(_.toString)
+  protected def explicit = getAsString("explicit").map(_.toBoolean).map(_.toString)
 
   override lazy val live = getFacetFromMap("live")
 
@@ -245,6 +245,59 @@ class R2ToFlexGalleryConversion(jsonMap : Map[String, Any], parseLiveData : Bool
       }
     </gallery>
   }
+}
+
+class R2ToFlexCartoonConversion(jsonMap : Map[String, Any], parseLiveData : Boolean)
+  extends R2ToFlexGalleryConversion(jsonMap, parseLiveData){
+
+  import scala.language.postfixOps
+
+  <picture story-bundle={storyBundleId orNull} cms-path={cmsPath orNull} notes={notes orNull} slug-word={slug orNull}
+           explicit={explicit orNull} expiry-date={scheduledExpiry orNull}
+           created-date={createdDate orNull} created-user={createdBy orNull} modified-date={modifiedDate orNull}
+           web-publication-date={webPublicationDate orNull}>
+
+    <tags>{for(tag <- tags) yield <tag id={tag}/> }</tags>
+    {r2PageId.map( pageId =>          <originalR2PageId>{pageId}</originalR2PageId>) orNull}
+    {r2ContentId.map( contentId =>    <originalR2ContentId>{contentId}</originalR2ContentId>) orNull}
+    {headline.map(h =>      <headline>{h}</headline>) orNull}
+    {strap.map(s =>         <strap>{s}</strap>) orNull}
+    {standfirst.map(s =>    <standfirst>{s}</standfirst>) orNull}
+    {byline.map(b =>        <byline>{b}</byline>) orNull}
+    {linktext.map(l =>      <linktext>{l}</linktext>) orNull}
+    {trailtext.map(t =>     <trail>{t}</trail>) orNull}
+    {thumbnailImageUrl.map(iu =>  <thumbnail-image-url>{iu}</thumbnail-image-url>) orNull}
+    {trailPictureId.map(tp =>     <trail-picture image-id={tp} />) orNull}
+    {largeTrailPictureId.map(ltp =>     <large-trail-picture image-id={ltp} />) orNull}
+    {if(!additionalPictures.isEmpty)
+    <pictures>
+      {additionalPictures.map{pic => {
+      pic.get("id").map{pid =>
+        <picture image-id={pid}>
+          {pic.get("caption").map{caption => <caption>{caption}</caption>} orNull}
+        </picture>} orNull
+    }}
+      }
+    </pictures>
+    }
+    <rights syndicationAggregate={syndicationAggregateFn orNull} subscriptionDatabases={subscriptionDatabasesFn orNull} developerCommunity={developerCommunityFn orNull} />
+    //expiry of rights and commercial expiry processing
+    {
+    val rightsExpiry = getRightsExpiry
+    val commercialExpiry = getCommercialExpiry
+    if(rightsExpiry.isDefined || commercialExpiry.isDefined){
+      <expiry>
+        {rightsExpiry.map(info => <rights expired={info._1.getOrElse(false).toString}
+                                          expiredAt={info._2 orNull}
+                                          scheduledExpiry={info._3 orNull}/>)         orNull}
+        {commercialExpiry.map(info => <commercial expired={info._1.getOrElse(false).toString}
+                                                  expiredAt={info._2 orNull}
+                                                  scheduledExpiry={info._3 orNull}/>) orNull}
+      </expiry>
+    }
+    }
+  </picture>
+
 }
 
 class R2ToFlexVideoConversion(jsonMap : Map[String, Any], parseLiveData : Boolean)
@@ -365,10 +418,20 @@ object R2ToFlexGalleryConversion{
 
 object R2ToFlexCartoonConversion{
 
-  def jsonMap(json : String) : Map[String, Any] = ???
+  def jsonMap(json : String) : Map[String, Any] = {
+    JacksMapper.readValue[Map[String, Any]](json)
+  }
 
-  def parseLiveData(json : Map[String, Any]) = ???
-  def parseDraftData(json : Map[String, Any]) = ???
+  def parseLiveData(json : Map[String, Any]) = {
+    val parsed = new R2ToFlexCartoonConversion(json, true)
+    Logger.debug(s"Produced live cartoon content XML:\n" + parsed.xml.toString())
+    parsed
+  }
+  def parseDraftData(json : Map[String, Any]) = {
+    val parsed = new R2ToFlexCartoonConversion(json, false)
+    Logger.debug(s"Produced draft cartoon content XML:\n" + parsed.xml.toString())
+    parsed
+  }
 }
 
 
