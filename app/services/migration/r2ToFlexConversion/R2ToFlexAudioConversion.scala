72aca19ef48742c381b89cea2b85cca15ca6e26b
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2015-11-09 13:20:30 +0000

    audio model mapping

--- app/services/migration/r2ToFlexConversion/R2ToFlexAudioConversion.scala
+++ app/services/migration/r2ToFlexConversion/R2ToFlexAudioConversion.scala
@@ -27,7 +27,7 @@ class R2ToFlexAudioConversion(jsonMap : Map[String, Any], parseLiveData : Boolea
 
   import scala.language.postfixOps
 
-  private def videoDuration : Option[(String, String)] = {
+  private def audioDuration : Option[(String, String)] = {
     for( mins <- getAs[Int]("durationMinutes"); secs <- getAs[Int]("durationSeconds"))
       yield (mins.toString, secs.toString)
   }
@@ -43,10 +43,10 @@ class R2ToFlexAudioConversion(jsonMap : Map[String, Any], parseLiveData : Boolea
       case _ =>       "no"
     })
 
-  private def videoSource = getAsString("source")
+  private def audioSource = getAsString("source")
 
   private def encodings : List[Map[String, String]] = {
-    val encodings: List[Map[String, Any]] = getAsMaps("videoEncodings", liveOrDraft).getOrElse(Nil)
+    val encodings: List[Map[String, Any]] = getAsMaps("audioFile", liveOrDraft).getOrElse(Nil)
     encodings.map(_.map(entry => entry._1 -> entry._2.toString))
   }
 
@@ -67,5 +67,52 @@ class R2ToFlexAudioConversion(jsonMap : Map[String, Any], parseLiveData : Boolea
 
   override lazy val draft = getFacetFromMap("draftVideo")
 
-  override lazy val xml = ??? //TODO
+  override lazy val xml = {
+    <audio story-bundle={storyBundleId orNull} cms-path={cmsPath orNull} notes={notes orNull} slug-word={slug orNull}
+           uk-only={ukOnly orNull} explicit={explicit orNull} expiry-date={scheduledExpiry orNull}
+           created-date={createdDate orNull} created-user={createdBy orNull} modified-date={modifiedDate orNull}
+           web-publication-date={webPublicationDate orNull}>
+
+      <tags>{for(tag <- tags) yield <tag id={tag}/> }</tags>
+      {r2PageId.map( pageId =>          <originalR2PageId>{pageId}</originalR2PageId>) orNull}
+      {r2ContentId.map( contentId =>    <originalR2ContentId>{contentId}</originalR2ContentId>) orNull}
+      {headline.map(h =>      <headline>{h}</headline>) orNull}
+      {strap.map(s =>         <strap>{s}</strap>) orNull}
+      {standfirst.map(s =>    <standfirst>{s}</standfirst>) orNull}
+      {byline.map(b =>        <byline>{b}</byline>) orNull}
+      {linktext.map(l =>      <linktext>{l}</linktext>) orNull}
+      {trailtext.map(t =>     <trail>{t}</trail>) orNull}
+      {audioDuration.map(d =>   <audio-duration minutes={d._1} seconds={d._2}/>) orNull}
+      {blockVideoAds.map(b =>  <block-audio-ads>{b}</block-audio-ads>) orNull}
+      {audioSource.map(as =>  <audio-source>{as}</audio-source>) orNull}
+      {encodings.map{ enc => {
+      <encoding>
+        {enc.get("videoFormat").map(f =>      <format>{f}</format>) orNull}
+        {enc.get("videoFileUrl").map(u =>     <video-file-url size-in-bytes={enc.get("sizeInBytes") orNull}>{u}</video-file-url>) orNull}
+        {enc.get("width").map(w =>            <width>{w}</width>) orNull}
+        {enc.get("height").map(h =>           <height>{h}</height>) orNull}
+        {enc.get("durationMinutes").map(m =>  <minutes>{m}</minutes>) orNull}
+        {enc.get("durationSeconds").map(s =>  <seconds>{s}</seconds>) orNull}
+      </encoding>
+    }}
+      }
+      {stillImageUrl.map(iu =>      <still-image-url>{iu}</still-image-url>) orNull}
+      {thumbnailImageUrl.map(iu =>  <thumbnail-image-url>{iu}</thumbnail-image-url>) orNull}
+      {embeddable.map(e =>          <embeddable>{e}</embeddable>) orNull}
+      {trailPictureId.map(tp =>     <trail-picture image-id={tp} />) orNull}
+      {largeTrailPictureId.map(ltp =>     <large-trail-picture image-id={ltp} />) orNull}
+      {if(!associatedPictures.isEmpty)
+      <pictures>
+        {associatedPictures.map{pic => {
+        pic.get("id").map{pid =>
+          <picture image-id={pid}>
+            {pic.get("caption").map{caption => <caption>{caption}</caption>} orNull}
+          </picture>} orNull
+      }}
+        }
+      </pictures>
+      }
+      {syndicateEncodings.map{se => <syndicate-encodings>{se}</syndicate-encodings>} orNull}
+    </audio>
+  }
 }
