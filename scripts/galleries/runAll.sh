263a9908b19403e242ad76f8355f8f20cc23404a
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2015-11-10 13:21:48 +0000

    galleries scripts

---
+++ scripts/galleries/runAll.sh
@@ -0,0 +1,20 @@
+# cmd to run 
+# nohup ./runAll.sh >output.txt & 
+
+./galleries_to_clean_ab	> output_ab.txt
+sleep 120
+./galleries_to_clean_ad	> output_ad.txt
+sleep 120
+./galleries_to_clean_af	> output_af.txt
+sleep 120
+./galleries_to_clean_ah	> output_ah.txt
+sleep 120
+./galleries_to_clean_aa	> output_aa.txt
+sleep 120
+./galleries_to_clean_ac	> output_ac.txt
+sleep 120
+./galleries_to_clean_ae	> output_ae.txt
+sleep 120
+./galleries_to_clean_ag	> output_ag.txt
+sleep 120
+./galleries_to_clean_ai > output_ai.txt
