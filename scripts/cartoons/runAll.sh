303b185ba88117307eb91a3bb339ae36fdfc33b7
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2015-11-10 09:01:46 +0000

    json test file and moved the cartoon scripts to the correct place

---
+++ scripts/cartoons/runAll.sh
@@ -0,0 +1,11 @@
+# cmd to run
+# nohup ./runAll.sh > output.txt &
+./cartoons_to_clean_aa	> output.aa.txt
+sleep 120
+./cartoons_to_clean_ab	> output_ab.txt
+sleep 120
+./cartoons_to_clean_ac	> output_ac.txt
+sleep 120
+./cartoons_to_clean_ad	> output_ad.txt
+sleep 120
+./cartoons_to_clean_ae > output_ae.txt
