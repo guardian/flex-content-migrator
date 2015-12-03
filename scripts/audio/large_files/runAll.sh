770626a9bef29bbaa27ff4e33a802075679bdde7
Thomas McKiernan <thomas.mckiernan.freelance@guardian.co.uk>
2015-12-03 15:12:37 +0000

    audio and article migration scripts

--- scripts/audio/large_files/runAll.sh
+++ scripts/audio/large_files/runAll.sh
@@ -2,7 +2,7 @@
 # cmd to run
 # nohup ./runAll.sh > output.txt &
 
-./large_file_part_aa    > output.aa.txt
+#./large_file_part_aa    > output.aa.txt
 ./large_file_part_ab	> output.ab.txt
 ./large_file_part_ac	> output.ac.txt
 ./large_file_part_ad	> output.ad.txt
