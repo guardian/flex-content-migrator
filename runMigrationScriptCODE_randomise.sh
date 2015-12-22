60bfd2ad192d0f5b5a0fefd170e04bc48db04985
Robert Rees <robert.rees@guardian.co.uk>
2015-12-22 12:21:35 +0000

    Parameterises the scripts and stores the output of the runs outside the source directory

--- runMigrationScriptCODE_randomise.sh
+++ runMigrationScriptCODE_randomise.sh
@@ -2,19 +2,19 @@
 echo off
 
 #CODE
-BATCH_SIZE=5
+BATCH_SIZE=${BATCH_SIZE:-5}
 NUMBER_OF_BATCHES=1
 
 #Chatterbox tag
-TAGIDS=28099
+TAGIDS=${TAGIDS:-28099}
 
 PREFIX="http://flexcontentmigrator.code.dev-gutools.co.uk/migrate/article?tagIds=$TAGIDS&batchSize=$BATCH_SIZE"
 
 echo "Migrating content to $PREFIX"
 
 TIMESTAMP="$(date +"%s")"
-OUTPUT_PATH=./migrationOutputCODE/$TIMESTAMP
-mkdir ./migrationOutputCODE
+OUTPUT_PATH=~/.migration/output/CODE/$TIMESTAMP
+mkdir -p ~/.migration/output/CODE
 mkdir $OUTPUT_PATH
 echo "Results in $OUTPUT_PATH"
 
@@ -24,13 +24,13 @@ echo "Results in $OUTPUT_PATH"
         do
 	    RANDOM_NUMBER=$(( ( RANDOM % 100 )  + 1 ))
 	    URL="$PREFIX&batchNumber=$RANDOM_NUMBER"
-          
+
 		echo migrating batch $i with $BATCH_SIZE content items : $URL
             BATCH_RESULTS=$OUTPUT_PATH/batch$i.txt
             curl -X POST $URL > $BATCH_RESULTS
             echo "results for batch $i in $BATCH_RESULTS"
             sleep 0.3
-        done    
+        done
 
 
 #Analyse the files to see if any had failures
@@ -39,5 +39,3 @@ echo ""
 echo "Problem batches..."
 
 grep -L "$EXPECTED_BATCH_RESULT" $OUTPUT_PATH/batch*.txt
-
- 
--- runMigrationScriptPROD_randomise.sh
+++ runMigrationScriptPROD_randomise.sh
@@ -4,9 +4,9 @@ echo off
 
 #PROD
 
-BATCH_SIZE=15                    # NO BIGGER THAN 30
-NUMBER_OF_BATCHES=200
-TAGIDS=26964  #MARKET FORCES			
+BATCH_SIZE=${BATCH_SIZE:-15}                    # NO BIGGER THAN 30
+NUMBER_OF_BATCHES=${NUMBER_OF_BATCHES:-200}
+TAGIDS=${TAGIDS:-6964}  #MARKET FORCES
 RANDOM_FACTOR=5                #Increase this value to migrate more randomly
 
 PREFIX="http://flexcontentmigrator.gutools.co.uk/migrate/article?tagIds=$TAGIDS&batchSize=$BATCH_SIZE"
@@ -18,8 +18,8 @@ read -p "Press [Enter] key to start **PROD** migration (random sequence)..."
 echo "Migrating content to $PREFIX"
 
 TIMESTAMP="$(date +"%s")"
-OUTPUT_PATH=./migrationOutput/$TIMESTAMP
-mkdir ./migrationOutput
+OUTPUT_PATH=~/migrationOutput/$TIMESTAMP
+mkdir ~/migrationOutput
 mkdir $OUTPUT_PATH
 echo "Results in $OUTPUT_PATH"
 
@@ -29,13 +29,13 @@ echo "Results in $OUTPUT_PATH"
         do
 	    RANDOM_NUMBER=$(( ( RANDOM % $RANDOM_FACTOR )  + 1 ))
 	    URL="$PREFIX&batchNumber=$RANDOM_NUMBER"
-            
+
             echo migrating batch $i with $BATCH_SIZE content items : $URL
             BATCH_RESULTS=$OUTPUT_PATH/batch$i.txt
             curl -X POST $URL > $BATCH_RESULTS
             echo "results for batch $i in $BATCH_RESULTS"
             sleep 0.5
-        done    
+        done
 
 
 #Analyse the files to see if any had failures
@@ -44,5 +44,3 @@ echo ""
 echo "Problem batches..."
 
 grep -L "$EXPECTED_BATCH_RESULT" $OUTPUT_PATH/batch*.txt
-
- 
