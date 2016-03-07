35836bd6a729b8fc52254003d1fb3d9fb8ba5cdc
Robert Rees <robert.rees@guardian.co.uk>
2016-03-07 16:27:04 +0000

    Crosswords: XML mapping
    This maps the R2 data to an XML format that can be used in the Flexible Content migrator.

--- app/services/migration/r2ToFlexConversion/R2ToFlexCrosswordConversion.scala
+++ app/services/migration/r2ToFlexConversion/R2ToFlexCrosswordConversion.scala
@@ -32,46 +32,18 @@ class R2ToFlexCrosswordConversion(jsonMap : Map[String, Any], parseLiveData : Bo
   override lazy val draft = getFacetFromMap("draft")
   override lazy val xml = {
     <crossword story-bundle={storyBundleId orNull}
-           created-date={createdDate orNull} created-user={createdBy orNull} modified-date={modifiedDate orNull}
-           web-publication-date={webPublicationDate orNull}>
+       cms-path={cmsPath orNull}
+       created-date={createdDate orNull} created-user={createdBy orNull}
+       modified-date={modifiedDate orNull}
+       web-publication-date={webPublicationDate orNull}>
 
       <tags>{for(tag <- tags) yield <tag id={tag}/> }</tags>
-      {r2PageId.map( pageId =>          <originalR2PageId>{pageId}</originalR2PageId>) orNull}
-      {r2ContentId.map( contentId =>    <originalR2ContentId>{contentId}</originalR2ContentId>) orNull}
-      {headline.map(h =>      <headline>{h}</headline>) orNull}
-      {strap.map(s =>         <strap>{s}</strap>) orNull}
-      {standfirst.map(s =>    <standfirst>{s}</standfirst>) orNull}
-      {byline.map(b =>        <byline>{b}</byline>) orNull}
-      {linktext.map(l =>      <linktext>{l}</linktext>) orNull}
-      {trailtext.map(t =>     <trail>{t}</trail>) orNull}
-      {trailPictureId.map(tp =>     <trail-picture image-id={tp} media-id={trailPictureMediaId orNull} />) orNull}
-      {largeTrailPictureId.map(ltp =>     <large-trail-picture image-id={ltp} media-id={largeTrailPictureMediaId orNull} />) orNull}
-      {if(!associatedPictures.isEmpty)
-      <pictures>
-        {associatedPictures.map{pic => {
-        pic.get("id").map{pid =>
-          <picture image-id={pid}>
-            {pic.get("caption").map{caption => <caption>{caption}</caption>} orNull}
-          </picture>} orNull
-      }}
-        }
-      </pictures>
-      }
-      <rights syndicationAggregate={syndicationAggregateFn orNull} subscriptionDatabases={subscriptionDatabasesFn orNull} developerCommunity={developerCommunityFn orNull} />
-      {
-      val rightsExpiry = getRightsExpiry
-      val commercialExpiry = getCommercialExpiry
-      if(rightsExpiry.isDefined || commercialExpiry.isDefined){
-        <expiry>
-          {rightsExpiry.map(info => <rights expired={info._1.getOrElse(false).toString}
-                                            expiredAt={info._2 orNull}
-                                            scheduledExpiry={info._3 orNull}/>)         orNull}
-          {commercialExpiry.map(info => <commercial expired={info._1.getOrElse(false).toString}
-                                                    expiredAt={info._2 orNull}
-                                                    scheduledExpiry={info._3 orNull}/>) orNull}
-        </expiry>
-      }
-      }
+
+      {r2PageId.map( pageId => <originalR2PageId>{pageId}</originalR2PageId>) orNull}
+      {r2ContentId.map( contentId => <originalR2ContentId>{contentId}</originalR2ContentId>) orNull}
+      {headline.map(h => <headline>{h}</headline>) orNull}
+      {linktext.map(l => <linktext>{l}</linktext>) orNull}
+      {trailtext.map(t => <trail>{t}</trail>) orNull}
     </crossword>
   }
 }
